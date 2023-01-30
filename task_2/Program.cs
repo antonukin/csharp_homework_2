4880cbc6714ec709a3af494a05f30c2fe58b3076
Anton <antonukin@icloud.com>
2023-01-30 16:04:08 +0300

    task_2 added

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int num = Int32.Parse(number);
+
+if(num / 100 < 1)
+{
+    Console.WriteLine($"В числе {num} меньше трех цифр");
+}
+if(num / 100 >= 1)
+{
+    Console.WriteLine($"Третьей цифрой числа {num} является {num / 100 % 10}");
+}
