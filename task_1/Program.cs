b3f5f0ed6d2e85badfc05a7ea80cb35eaeffc58c
Anton <antonukin@icloud.com>
2023-01-30 15:58:02 +0300

    task_1 added

---
+++ task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int RandomNumber()
+{
+    int num = new Random().Next(100, 1000);
+    return num;
+}
+
+int SecondDigit(int number)
+{
+    int secDig = number / 10 % 10;
+    return secDig;
+}
+
+int number = RandomNumber();
+int secondDig = SecondDigit(number);
+Console.WriteLine($"Дано число {number}, вторая цифра числа {secondDig}");
