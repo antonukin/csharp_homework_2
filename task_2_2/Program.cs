e58c404854234cb088041f07653d1ca62a9d27f6
Anton <antonukin@icloud.com>
2023-01-30 17:10:12 +0300

    task_2_2 added

---
+++ task_2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+string randomNumber = Console.ReadLine();
+
+if (randomNumber.Length > 2)
+{
+    Console.WriteLine($"Третьей цифрой числа {randomNumber} является {randomNumber[2]}");
+}
+else
+{
+    Console.WriteLine($"В числе {randomNumber} меньше трех цифр");
+}
