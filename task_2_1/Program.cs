d7f6665fe5ce9b67b10d8c8bf63d48bed77c31e3
Anton <antonukin@icloud.com>
2023-01-30 16:46:48 +0300

    task_2_1 added

---
+++ task_2_1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int RandomNumber()
+{
+    int num = new Random().Next(1, 1000000);
+    return num;
+}
+
+int number = RandomNumber();
+
+if(number / 100 < 1)
+{
+    Console.WriteLine($"В числе {number} меньше трех цифр");
+}
+if(number >= 100 && number < 1000)
+{
+    Console.WriteLine($"Третьей цифрой числа {number} является {number % 10}");
+}
+if(number >= 1000 && number < 10000)
+{
+    Console.WriteLine($"Третьей цифрой числа {number} является {number % 100 / 10}");
+}
+if(number >= 10000 && number < 100000)
+{
+    Console.WriteLine($"Третьей цифрой числа {number} является {number % 1000 / 100}");
+}
+if(number >= 100000 && number < 1000000)
+{
+    Console.WriteLine($"Третьей цифрой числа {number} является {number % 10000 / 1000}");
+}
